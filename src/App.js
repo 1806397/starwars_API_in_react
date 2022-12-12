c05c8e1f73b2698b09dc0fc7c495b19f4e67cee7
Piyush Singh <1806397@kiit.ac.in>
2022-12-12 15:43:00 +0530

    1st commit:starwars API called

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const [movies, setMovie] = useState([]);
+  function fetchMovieHandler() {
+    fetch("https://swapi.dev/api/films/")
+      .then((resp) => {
+        return resp.json();
+      })
+      .then((data) => {
+        const transformedMovies = data.results.map((movieData) => {
+          return {
+            //changing data name of API
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovie(transformedMovies);
+      })
+      .catch();
+  }
+
+  return (
+    <>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </>
+  );
+}
+
+export default App;
