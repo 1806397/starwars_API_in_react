56a0e83e380eff3a8ff3f40b0d64e377d6d3295b
Piyush Singh <1806397@kiit.ac.in>
2022-12-12 16:38:07 +0530

    4th commit:Minor changes

--- src/App.js
+++ src/App.js
@@ -5,32 +5,45 @@ import "./App.css";
 
 function App() {
   const [movies, setMovie] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
   async function fetchMovieHandler() {
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch("https://swapi.dev/api/films/");
+      if (!response.ok) {
+        throw new Error("Something went wrong.\n Please try again later");
+      }
+      const data = await response.json();
+      const transformedMovies = data.results.map((movieData) => {
+        return {
+          //changing data name of API
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovie(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
 
-    const transformedMovies = data.results.map((movieData) => {
-      return {
-        //changing data name of API
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
-    setMovie(transformedMovies);
-  }
-  function closeMoive() {
-    setMovie([]);
-  }
   return (
     <>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
-        <button onClick={closeMoive}>Close Movie</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
+        {!isLoading && movies.length === 0 && !error && (
+          <p>No movies found...</p>
+        )}
+        {isLoading && <p>Loading...</p>}
+        {!isLoading && error && <p>{error}</p>}
       </section>
     </>
   );
